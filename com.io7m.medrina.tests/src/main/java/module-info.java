3dd8f3b066243b079cd9812d5e552bdb4af0f2ca
Mark Raynsford <code@io7m.com>
2024-10-21 19:42:20 +0000

    Upgrade junit, use junit BOM.

--- com.io7m.medrina.tests/src/main/java/module-info.java
+++ com.io7m.medrina.tests/src/main/java/module-info.java
@@ -40,10 +40,11 @@ open module com.io7m.medrina.tests
   requires org.apiguardian.api;
   requires org.slf4j;
 
-  requires transitive org.junit.jupiter.api;
-  requires transitive org.junit.jupiter.engine;
-  requires transitive org.junit.platform.commons;
-  requires transitive org.junit.platform.engine;
+  requires org.junit.jupiter.api;
+  requires org.junit.jupiter.engine;
+  requires org.junit.platform.commons;
+  requires org.junit.platform.engine;
+  requires org.junit.platform.launcher;
 
   provides net.jqwik.api.providers.ArbitraryProvider
     with com.io7m.medrina.tests.MActionNames,
